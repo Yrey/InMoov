f9598306d14b3c34d995e9b4f856d6475eb11f78
Yann Briançon <briyan@sfr.fr>
2017-11-16 09:31:17 +0100

    Add the connection to the Arduino board and the control of the servos

--- Hand/src/control.py
+++ Hand/src/control.py
@@ -14,7 +14,12 @@ arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap
-import math
+import pyfirmata
+
+# Adjust that the port match your system, see samples below:
+# On Linux: /dev/tty.usbserial-A6008rIF, /dev/ttyACM0, 
+# On Windows: \\.\COM1, \\.\COM2
+PORT = '/dev/ttyACM0'
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
@@ -23,7 +28,7 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the left range into a 0-1 range (float)
     valueScaled = float(value - leftMin) / float(leftSpan)
     # Convert the 0-1 range into a value in the right range.
-    return rightMin + (valueScaled * rightSpan)
+    return rightMax - (valueScaled * rightSpan)
 
 def getPosHand(hand):
     pos = hand.palm_position
@@ -48,16 +53,26 @@ def correctionMinMax(distance, MinMax):
 
 
 def main():
+    # Creates a new board 
+    board = pyfirmata.Arduino(PORT)
+    
+    # set up fingers as Servo Output
+    thumb = board.get_pin('d:6:s')
+    index = board.get_pin('d:5:s')
+    middle = board.get_pin('d:8:s')
+    ring = board.get_pin('d:3:s')
+    pinky = board.get_pin('d:2:s')  
+
     controller=Leap.Controller()
     while(not controller.is_connected):
         print("Waiting for connection")
-    print controller.is_connected
+    print(controller.is_connected)
 
-    distThumb_minmax = [math.inf , 1]
-    distIndex_minmax = [math.inf , 1]
-    distMiddle_minmax = [math.inf , 1]
-    distRing_minmax = [math.inf , 1]
-    distPinky_minmax = [math.inf , 1]
+    distThumb_minmax = [float("inf"), 1]
+    distIndex_minmax = [float("inf"), 1]
+    distMiddle_minmax = [float("inf"), 1]
+    distRing_minmax = [float("inf"), 1]
+    distPinky_minmax = [float("inf"), 1]
 
 
     while(controller.is_connected):
@@ -89,15 +104,19 @@ def main():
         distPinky_minmax = correctionMinMax(distPinky,distPinky_minmax)
     
         ###Calcul commande
-        AngleThumb = translate(distThumb,distThumb_minmax[0],distThumb_minmax[1],0,180)
-        #print(AngleThumb)
-        AngleIndex = translate(distIndex,distIndex_minmax[0],distIndex_minmax[1],0,180)
-        AngleMiddle = translate(distMiddle,distMiddle_minmax[0],distMiddle_minmax[1],0,180)
-        AngleRing = translate(distRing,distRing_minmax[0],distRing_minmax[1],0,180)
-        AnglePinky = translate(distPinky,distPinky_minmax[0],distPinky_minmax[1],0,180)
+        AngleThumb = translate(distThumb,distThumb_minmax[0],distThumb_minmax[1],30,120)
+        AngleIndex = translate(distIndex,distIndex_minmax[0],distIndex_minmax[1],0,110)
+        AngleMiddle = translate(distMiddle,distMiddle_minmax[0],distMiddle_minmax[1],0,120)
+        AngleRing = translate(distRing,distRing_minmax[0],distRing_minmax[1],20,170)
+        AnglePinky = translate(distPinky,distPinky_minmax[0],distPinky_minmax[1],30,180)
 
         commande = [AngleThumb, AngleIndex, AngleMiddle, AngleRing, AnglePinky]
         print(commande)
+        thumb.write(AngleThumb)
+        index.write(AngleIndex)
+        middle.write(AngleMiddle)
+        ring.write(AngleRing)
+        pinky.write(AnglePinky)
 
 if __name__ == '__main__':
     main()
