e1f618839c7fd74e9c5ccd13e242d22ced521099
Yann Briançon <briyan@sfr.fr>
2017-11-16 09:23:59 +0100

    Clean the code and generalize to all the fingers

--- Hand/src/control.py
+++ Hand/src/control.py
@@ -9,7 +9,7 @@
 """
 
 import Leap   #, sys, thread, time
-import serial
+import math
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
@@ -20,52 +20,79 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
+def getPosHand(hand):
+    pos = hand.palm_position
+    return [ pos[0] , pos[1] , pos[2] ]
 
+def getPosFinger(fingers, indice):
+    tip = fingers[indice].joint_position(3)
+    return [ tip[0], tip[1], tip[2]]
 
+def getDistFinger(matHand, matFinger):
+    return ((matHand[0] - matFinger[0])**2 + (matHand[1] - matFinger[1])**2 + (matHand[2] - matFinger[2])**2 )
 
-controller=Leap.Controller()
-while(not controller.is_connected):
-    print("Waiting for connection")
-print controller.is_connected
-#frame = 0
-distThumb_max=1
-distThumb_min=100000
-
-SERIAL = serial.Serial("/dev/ttyACM1", 9600, timeout=1)
-
-
-while(controller.is_connected):
-    #k+=1
-    #print(k)
-    #if(controller.is_connected): #controller is a Leap.Controller object
-    frame = controller.frame() #The latest frame
-    #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
-    #          frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
-
+def correctionMinMax(distance, MinMax):
+    min = MinMax[0]
+    max = MinMax[1]
+    if distance>max:
+            max=distance
+    if distance<min:
+        min=distance
+    return [min, max]
     
-    hand = controller.frame().hands.rightmost
-    handPosition = hand.palm_position
-    MatHand = [ handPosition[0] , handPosition[1] , handPosition[2] ]
-    #print(MatHand)
+
+
+def main():
+    controller=Leap.Controller()
+    while(not controller.is_connected):
+        print("Waiting for connection")
+    print controller.is_connected
+
+    distThumb_minmax = [math.inf , 1]
+    distIndex_minmax = [math.inf , 1]
+    distMiddle_minmax = [math.inf , 1]
+    distRing_minmax = [math.inf , 1]
+    distPinky_minmax = [math.inf , 1]
+
+
+    while(controller.is_connected):
+        ###initialisation
+        frame = controller.frame()
+        hand = frame.hands.rightmost
     
-    fingers = hand.fingers
-    thumbTip=fingers[0].joint_position(3)
-    MatThumb = [ fingers[0].joint_position(3)[0], fingers[0].joint_position(3)[1], fingers[0].joint_position(3)[2] ]
-    #print(MatThumb)
+        ###Recuperation des objets
+        MatHand = getPosHand(hand)
+        #print(MatHand)
+        MatThumb = getPosFinger(hand.fingers,0)
+        #print(MatThumb)
+        MatIndex = getPosFinger(hand.fingers,1)
+        MatMiddle = getPosFinger(hand.fingers,2)
+        MatRing = getPosFinger(hand.fingers,3)
+        MatPinky = getPosFinger(hand.fingers,4)
     
+        ###Calcul distances
+        distThumb = getDistFinger(MatHand,MatThumb)
+        distThumb_minmax = correctionMinMax(distThumb,distThumb_minmax)
+        #print(distThumb)
+        distIndex = getDistFinger(MatHand,MatIndex)
+        distIndex_minmax = correctionMinMax(distIndex,distIndex_minmax)
+        distMiddle = getDistFinger(MatHand,MatMiddle)
+        distMiddle_minmax = correctionMinMax(distMiddle,distMiddle_minmax)
+        distRing = getDistFinger(MatHand,MatRing)
+        distRing_minmax = correctionMinMax(distRing,distRing_minmax)
+        distPinky = getDistFinger(MatHand,MatPinky)
+        distPinky_minmax = correctionMinMax(distPinky,distPinky_minmax)
     
-    distThumb = ((MatHand[0] - MatThumb[0])**2 + (MatHand[1] - MatThumb[1])**2 + (MatHand[2] - MatThumb[2])**2 )
-    print(distThumb)
-    
-    if distThumb>distThumb_max:
-        distThumb_max=distThumb
-    if distThumb<distThumb_min:
-        distThumb_min=distThumb
-    AngleThumb = translate(distThumb,distThumb_min,distThumb_max,0,180)
-    print(AngleThumb)
-    strAngleThumb = str(AngleThumb)
-    SERIAL.write(strAngleThumb)
-    
+        ###Calcul commande
+        AngleThumb = translate(distThumb,distThumb_minmax[0],distThumb_minmax[1],0,180)
+        #print(AngleThumb)
+        AngleIndex = translate(distIndex,distIndex_minmax[0],distIndex_minmax[1],0,180)
+        AngleMiddle = translate(distMiddle,distMiddle_minmax[0],distMiddle_minmax[1],0,180)
+        AngleRing = translate(distRing,distRing_minmax[0],distRing_minmax[1],0,180)
+        AnglePinky = translate(distPinky,distPinky_minmax[0],distPinky_minmax[1],0,180)
+
+        commande = [AngleThumb, AngleIndex, AngleMiddle, AngleRing, AnglePinky]
+        print(commande)
     
     
     
